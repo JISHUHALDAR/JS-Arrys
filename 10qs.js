6b55e9fd84eb2c4fc42be2a63431e0c8d3c484cf
JISHUHALDAR <104211124+JISHUHALDAR@users.noreply.github.com>
2022-06-19 20:14:05 +0530

    Add files via upload

---
+++ 10qs.js
@@ -0,0 +1,21 @@
+// Min and Max
+// Congratulations on making up to this question. Let us give you a fairly simple array problem to solve. If you know how to iterate through the array, you will easily be able to solve this.
+// The problem statement is simple- given 
+// N
+//  elements, find the minimum and maximum numbers among those elements
+
+ 
+function arrayMin(arr) {
+  var b=Math.min(...arr);
+  return b;
+
+}
+
+function arrayMax(arr) {
+var a=Math.max(...arr);
+ return a;
+
+}
+
+
+
